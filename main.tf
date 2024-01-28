8d9652789a7f6931c153b6ca7399a7a706c2a2d8
gbakthavatchalam <26194642+gbakthavatchalam@users.noreply.github.com>
2024-01-28 13:15:26 +0100

    Fix syntax error

--- main.tf
+++ main.tf
@@ -58,12 +58,11 @@ resource "aws_security_group_rule" "web_ingress_https" {
 
 
 resource "aws_security_group_rule" "web_egress" {
-  type        = "egress""
+  type        = "egress"
   protocol    = "-1"
   from_port   = 0
   to_port     = 0
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.web.id
-
 }
