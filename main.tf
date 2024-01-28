85ef761fd35b3dcfbdbf846bddf478a9761a95bc
gbakthavatchalam <26194642+gbakthavatchalam@users.noreply.github.com>
2024-01-28 13:11:46 +0100

    Allow inbound/outbound traffic

--- main.tf
+++ main.tf
@@ -18,7 +18,52 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  vpc_security_group_id = [aws_security_group.web.id]
+
   tags = {
     Name = "HelloWorld"
   }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "web" {
+  name        = "web"
+  description = "allow inbound traffic on http/https and outbound for everything"
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "web_ingress_http" {
+  type        = "ingress"
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port   = 80
+  to_port     = 80
+
+  security_group_id = aws_security_group.web.id
+}
+
+resource "aws_security_group_rule" "web_ingress_https" {
+  type        = "ingress"
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port   = 443
+  to_port     = 443
+
+  security_group_id = aws_security_group.web.id
+}
+
+
+resource "aws_security_group_rule" "web_egress" {
+  type        = "egress""
+  protocol    = "-1"
+  from_port   = 0
+  to_port     = 0
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.web.id
+
 }
